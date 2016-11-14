82b11ea5fa85d4b8b81c1cc41e0a9dc1393d1680
Kevin Zhang <zarknight@gmail.com>
2016-11-14 08:40:57 +0800

    update

--- client/utils/util.js
+++ client/utils/util.js
@@ -1,22 +1,5 @@
 import Promise from '../libs/bluebird.min'
 
-function formatTime(date) {
-  let year = date.getFullYear()
-  let month = date.getMonth() + 1
-  let day = date.getDate()
-
-  let hour = date.getHours()
-  let minute = date.getMinutes()
-  let second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
-
 function wxPromisify(fn) {
   return function (obj = {}) {
     return new Promise((resolve, reject) => {
@@ -34,6 +17,5 @@ function wxPromisify(fn) {
 }
 
 module.exports = {
-  formatTime: formatTime,
   wxPromisify: wxPromisify
 }
